317f4ce1a4f5a89e5874df24e10c15ba86ac9eb3
arpan-asus <iamarpan15@gmail.com>
2024-04-24 20:11:43 +0545

    nowhere

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello Nepal");
+        System.out.println("Hello Nepal, I am from nowhere");
     }
 }
